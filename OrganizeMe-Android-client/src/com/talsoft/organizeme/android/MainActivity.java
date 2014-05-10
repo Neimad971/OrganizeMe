1030160d8fe305e8d4a1ede148b1540f4d702e4b
neimad90 <damientalbot26@gmail.com>
2014-05-10 12:27:54 +0200

    build success

--- OrganizeMe-Android-client/src/com/talsoft/organizeme/android/MainActivity.java
+++ OrganizeMe-Android-client/src/com/talsoft/organizeme/android/MainActivity.java
@@ -1,5 +1,7 @@
 package com.talsoft.organizeme.android;
 
+import com.talsoft.organizeme_android_client.R;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
---
+++ OrganizeMe-Android-client/target/generated-sources/r/com/talsoft/organizeme_android_client/BuildConfig.java
@@ -0,0 +1,5 @@
+package com.talsoft.organizeme_android_client;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = true;
+}
